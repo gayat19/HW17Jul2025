c1d0c668dea6a94d94e42c24e0c44a9eb700f783
unknown <gayat_19@yahoo.com>
2025-08-14 09:25:04 +0530

    CSR-react app'

---
+++ FE/Day4/employee-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Components/Login/Login';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ FE/Day4/employee-app/src/Models/loginerror.model.js
@@ -0,0 +1,4 @@
+export class LoginErrorModel{
+    username="";
+    password="";
+}
